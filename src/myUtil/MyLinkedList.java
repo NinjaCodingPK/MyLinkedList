097dc6f23db57084465d0af298c23f65bfd80439
wookie <kononenkoa995@ukr.net>
2016-06-08 21:18:07 +0300

    First commit.

--- src/myUtil/MyLinkedList.java
+++ src/myUtil/MyLinkedList.java
@@ -1,308 +1,284 @@
 package myUtil;
 
 import java.util.*;
-import java.util.function.UnaryOperator;
 
 /**
  * Created by wookie on 6/3/16.
  */
-public class MyLinkedList<T> implements List<T>, Queue<T> {
-    private int size = 0;
+public class MyLinkedList<T> implements Iterable<T> {
+    private Node<T> first;
+    private Node<T> last;
+    private int modCount;
+    private int size;
+
     private class Node<T> {
+        private T data;
         private Node next;
         private Node prev;
-        private T value;
 
-        private Node() {
+        public Node(){}
 
-        }
-
-        private Node(T value) {
-            this.value = value;
+        public Node(T data) {
+            this.data = data;
         }
     }
-    private Node<T> start = new Node<>();
-    private Node<T> end = new Node<>();
-    private int modCount;
 
-    public MyLinkedList() {
-        start.next = end;
-        end.prev = start;
+    public T getFirst() {
+        return (T) first.data;
     }
 
-    public void push_front(T o) {
-        Node<T> temp = start.next;
-        start.next = new Node<>(o);
-        start.next.prev = start;
-        start.next.next = temp;
-        temp.prev = start.next;
-        size++;
+    public T getLast() {
+        return (T) last.data;
+    }
+
+
+    public void addFirst(T element) {
         modCount++;
-    }
-
-    public void push_back(T o) {
-        Node<T> temp = end.prev;
-        end.prev = new Node<>(o);
-        end.prev.next = end;
-        end.prev.prev = temp;
-        temp.next = end.prev;
+        if (first == null) {
+            first = new Node(element);
+            last = first;
+            size++;
+            return;
+        }
+        Node node = new Node(element);
+        first.prev = node;
+        node.next = first;
+        first = node;
         size++;
+    }
+
+    public boolean add(T element) {
         modCount++;
-    }
-
-    public T pop_front() {
-        if(size > 0) {
-            T value = (T) start.next.value;
-            start.next = start.next.next;
-            start.next.prev = start;
-            size--;
-            modCount++;
-            return value;
+        if (first == null) {
+            first = new Node(element);
+            last = first;
+            size++;
+            return false;
         }
-        else
-            return null;
-    }
-
-    public T pop_back() {
-        if(size > 0) {
-            T value = (T) end.prev.value;
-            end.prev = end.prev.prev;
-            end.prev.next = end;
-            size--;
-            modCount++;
-            return value;
+        Node temp = first;
+        while (temp.next != null) {
+            temp = temp.next;
         }
-        else
-            return null;
-    }
-
-    @Override
-    public boolean offer(T t) {
+        temp.next = new Node(element);
+        last = temp.next;
+        last.prev = temp;
+        size++;
         return false;
     }
 
-    @Override
-    public T remove() {
-        return pop_front();
+    public void add(int index, T element) {
+        if (index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
+        }
+        if (index == 0) {
+            addFirst(element);
+            return;
+        }
+        if (index == size()) {
+            add(element);
+            return;
+        }
+        modCount++;
+        Node temp = getNode(index);
+
+        Node node = new Node(element);
+        temp.next.prev = node;
+        node.next = temp.next;
+        node.prev = temp;
+        temp.next = node;
+        size++;
     }
 
-    @Override
-    public T poll() {
-        return null;
+    public T get(int index) {
+        checkIndex(index);
+        Node temp = getNode(index);
+        return (T) temp.data;
     }
 
-    @Override
-    public T element() {
-        return null;
+    private Node getNode(int index) {
+        int i;
+        Node temp;
+        if (index < size()/2) {
+            i = 0;
+            temp = first;
+            while (i != index) {
+                temp = temp.next;
+                i++;
+            }
+            //* return (T) temp.data;
+        } else {    // index >= size() / 2
+            i = size() - 1;
+            temp = last;
+            while (i != index) {
+                temp = temp.prev;
+                i--;
+            }
+
+        }
+        return temp;
     }
 
-    @Override
-    public T peek() {
-        return null;
+    public void set(int index, T value) {
+        checkIndex(index);
+        modCount++;
+        Node temp = getNode(index);
+        temp.data = value;
     }
 
-    @Override
-    public void replaceAll(UnaryOperator<T> operator) {
-
+    public T removeFirst() {
+        if (first == null) {
+            throw new UnsupportedOperationException("The list is empty");
+        } else if (last == first) {
+            size--;
+            Node temp = first;
+            first = last = null;
+            return (T) temp.data;
+        }
+        modCount++;
+        Node temp = first;
+        first = first.next;
+        first.prev = null;
+        size--;
+        return (T) temp.data;
     }
 
-    @Override
-    public void sort(Comparator<? super T> c) {
-
+    public T removeLast() {
+        if (first == null && last == null) {
+            throw new UnsupportedOperationException("The list is empty");
+        } else if (last == first) {
+            size--;
+            Node temp = first;
+            first = last = null;
+            return (T) temp.data;
+        }
+        modCount++;
+        Node temp = last;
+        last = last.prev;
+        last.next = null;
+        size--;
+        return (T) temp.data;
     }
 
-    @Override
-    public Spliterator<T> spliterator() {
-        return null;
+    public boolean contains(T element) {
+        Node temp = first;
+        while (temp != null) {
+            if (temp.data.equals(element)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
     }
 
-    @Override
     public int size() {
         return size;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return this.listIterator();
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T1> T1[] toArray(T1[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(T t) {
-        this.push_back(t);
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends T> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    private boolean checkIndex(int index) {
-        return ((index >= 0) && (index < size));
-    }
-
-    private Node<T> getNode(int index) {
-        if(!checkIndex(index)) {
-            throw new IllegalArgumentException();
+    public void print() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
         }
+        System.out.println();
+    }
 
-        Node<T> node;
-        if(index < (double)size/2) {
-            node = start.next;
-            for(int i = 0; i < index; i++) {
-                node = node.next;
-            }
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
         }
-        else {
-            node = end.prev;
-            for(int i = size-1; i > index; i--) {
-                node = node.prev;
-            }
-        }
-        return node;
     }
 
-    @Override
-    public T get(int index) {
-        return getNode(index).value;
-    }
-
-    @Override
-    public void add(int index, T element) {
-
-    }
-
-    @Override
-    public T remove(int index) {
-        return null;
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public ListIterator<T> listIterator() {
+    public ListIterator<T> iterator() {
         return new ListIterator<T>() {
-            Node<T> current = start.next;
+            int modCount = 0;
+            Node<T> current = first;
+            int currentIndex = 0;
+            Node<T> justReturned = null;
+
             @Override
             public boolean hasNext() {
-                return current != end;
+                return  (current != null && current.next != null);
             }
 
             @Override
             public T next() {
+                justReturned = current;
                 current = current.next;
-                return (T)current.prev.value;
+                currentIndex++;
+
+                return (T) justReturned.data;
             }
 
             @Override
             public boolean hasPrevious() {
-                return current != start;
+                return (current != null && current.prev != null);
             }
 
             @Override
             public T previous() {
+                justReturned = current;
                 current = current.prev;
-                return (T)current.prev.value;
+                currentIndex--;
+                return (T) justReturned.data;
             }
 
             @Override
             public int nextIndex() {
-                return 0;
+                return currentIndex++;
             }
 
             @Override
             public int previousIndex() {
-                return 0;
+                return currentIndex--;
             }
 
             @Override
             public void remove() {
-                current.prev.next = current.next;
-                current.next.prev = current.prev;
+                checkMods();
+                modCount++;
+                if (justReturned == null) {
+                    throw new IndexOutOfBoundsException("Just Returned is null");
+                }
+                if (justReturned == first) {
+                    MyLinkedList.this.removeFirst();
+                } else if (justReturned == last) {
+                    MyLinkedList.this.removeLast();
+                } else {
+                    justReturned.next.prev = justReturned.prev;
+                    justReturned.prev.next = justReturned.next;
+                    justReturned = null;
+                }
             }
 
             @Override
             public void set(T t) {
-                current.value = t;
+                checkMods();
+                modCount++;
+                justReturned.data = t;
             }
 
             @Override
             public void add(T t) {
+                checkMods();
+                modCount++;
+                if (justReturned == first) {
+                    MyLinkedList.this.addFirst(t);
+                } else if (justReturned == last) {
+                    MyLinkedList.this.add(t);
+                } else {
+                    Node node = new Node(t);
+                    justReturned.next.prev = node;
+                    node.next = justReturned.next;
+                    justReturned.next = node;
+                    node.prev = justReturned;
+                }
+            }
 
+            private void checkMods() {
+                if (modCount != MyLinkedList.this.modCount) throw new ConcurrentModificationException();
             }
         };
     }
 
-    @Override
-    public ListIterator<T> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<T> subList(int fromIndex, int toIndex) {
-        return null;
-    }
-
-    @Override
-    public T set(int index, T element) {
-        return null;
-    }
 }
